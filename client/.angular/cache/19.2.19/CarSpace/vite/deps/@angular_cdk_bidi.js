3002d1904c25dcc252181926ee53d9e94c996873
shayne-lwl <shayne.weiliang@gmail.com>
2026-02-21 21:10:59 +0800

    refactor: restructure project to have a client and server directory

---
+++ client/.angular/cache/19.2.19/CarSpace/vite/deps/@angular_cdk_bidi.js
@@ -0,0 +1,16 @@
+import {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+} from "./chunk-JR53LCQS.js";
+import "./chunk-FYEQ44HC.js";
+import "./chunk-MPY72NOG.js";
+import "./chunk-CB336HSR.js";
+import "./chunk-S35MAB2V.js";
+export {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+};
